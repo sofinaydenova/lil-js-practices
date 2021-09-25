6323fbe486cba671837d3eae948879a7d739703a
__________sofi____________(✧ω✧) <53216481+sofinaydenova@users.noreply.github.com>
2021-09-25 06:56:22 +0300

    anonymous function + arrow function

--- daysoff.js
+++ daysoff.js
@@ -8,3 +8,14 @@ const dayOff = function(day) {
 
 dayOff('Tuesday')
 console.log(dayOff('Tuesday'))
+
+// arrow function
+const dayOff = (day) => {
+  if(day === 'Monday') {
+    return true }
+  else {
+    return false }  
+}
+
+dayOff('Friday')
+console.log(dayOff('Friday'))
